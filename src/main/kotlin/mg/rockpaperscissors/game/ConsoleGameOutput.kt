536db37dace83efe70eef04bc14be32861187470
marcogramm <mail.mgramm@gmail.com>
2020-07-26 20:05:09 +0200

    Implemented the ConsoleGameOutput class

--- src/main/kotlin/mg/rockpaperscissors/game/ConsoleGameOutput.kt
+++ src/main/kotlin/mg/rockpaperscissors/game/ConsoleGameOutput.kt
@@ -2,9 +2,27 @@ package mg.rockpaperscissors.game
 
 import mg.rockpaperscissors.domain.Round
 
-class ConsoleGameOutput: GameOutput {
+class ConsoleGameOutput : GameOutput {
+    companion object {
+        private const val SEPERATOR = "============================================"
+    }
+
     override fun outputRound(round: Round) {
-        TODO("Not yet implemented")
+        reportActionsForRound(round);
+        reportResultsForRound(round)
+        println(SEPERATOR)
+    }
+
+    private fun reportActionsForRound(round: Round) {
+        println("Ready, Steady, Go! Show your actions!")
+        round.actions
+                .forEach { (player, action) -> println("${player.name} shows... $action! ") }
+    }
+
+    private fun reportResultsForRound(round: Round) {
+        println("The round is over! The results are:")
+        round.results
+                .forEach { (player, result) -> println("${player.name}: $result") }
     }
 
     override fun outputSummary(rounds: List<Round>) {
