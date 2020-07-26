de4f5e4349365728d22e04ca85cef117315821f9
marcogramm <mail.mgramm@gmail.com>
2020-07-26 21:07:15 +0200

    added a simple Test for RoundTest.kt

---
+++ src/test/kotlin/mg/rockpaperscissors/domain/RoundTest.kt
@@ -0,0 +1,31 @@
+package mg.rockpaperscissors.domain
+
+import mg.rockpaperscissors.domain.strategy.RandomChoiceStrategy
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class RoundTest {
+
+    @Test
+    fun addActionAndResultForPlayer() {
+        var round: Round = Round()
+        val player: Player = Player("test", RandomChoiceStrategy())
+        val expectedAction = Action.ROCK
+        val expectedResult = Result.WIN
+
+        Assertions.assertThat(round.results.containsKey(player)).isFalse()
+        Assertions.assertThat(round.actions.containsKey(player)).isFalse()
+
+        round.addActionAndResultForPlayer(player, expectedAction, expectedResult)
+
+        Assertions.assertThat(round.results.containsKey(player)).isTrue()
+        Assertions.assertThat(round.actions.containsKey(player)).isTrue()
+
+        val actualAction = round.actions[player]
+        val actualResult = round.results[player]
+
+        Assertions.assertThat(actualAction).isEqualTo(expectedAction)
+        Assertions.assertThat(actualResult).isEqualTo(expectedResult)
+
+    }
+}
