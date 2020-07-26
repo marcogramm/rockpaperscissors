657f1481831cdebfbc125b9acd8467958132a1a1
marcogramm <mail.mgramm@gmail.com>
2020-07-26 13:32:02 +0200

    Added Documentation for the Player class

--- src/main/kotlin/mg/rockpaperscissors/domain/Player.kt
+++ src/main/kotlin/mg/rockpaperscissors/domain/Player.kt
@@ -2,6 +2,17 @@ package mg.rockpaperscissors.domain
 
 import mg.rockpaperscissors.domain.strategy.Strategy
 
+/**
+ * Represents a player in the Game.
+ * A Player is currently only identified by its name.
+ *
+ * Also, a player is using a certain Strategy to be able to decide
+ * about it's next Action
+ */
 class Player(val name: String, private var strategy: Strategy) {
+    /**
+     * Returns the next Action of this player, according to the Strategy used by this Player
+     * @return The next Action of this Player
+     */
     fun nextAction(): Action = strategy.nextAction()
 }
