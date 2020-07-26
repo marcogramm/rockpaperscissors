06cd518eca828e7a374d8d1f3d3d223d007ab13b
marcogramm <mail.mgramm@gmail.com>
2020-07-26 13:18:45 +0200

    Added Documentation for the Action enum

--- src/main/kotlin/mg/rockpaperscissors/domain/Action.kt
+++ src/main/kotlin/mg/rockpaperscissors/domain/Action.kt
@@ -1,5 +1,8 @@
 package mg.rockpaperscissors.domain
 
+/**
+ * Represents a Action or "Move" in the Rock Paper Scissors Game
+ */
 enum class Action {
     ROCK {
         override fun beats(action: Action) = when (action) {
@@ -20,5 +23,10 @@ enum class Action {
         }
     };
 
+    /**
+     * Checks if this Action beats the given Action
+     * @param action The action to check against
+     * @return true if this Action beats the Action given as a parameter. False otherwise
+     */
     abstract fun beats(action: Action): Boolean
 }
