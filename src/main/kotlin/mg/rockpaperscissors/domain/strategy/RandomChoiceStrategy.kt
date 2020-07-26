52c92eb9f95522e8c0235d0dd3ee069244e7128c
marcogramm <mail.mgramm@gmail.com>
2020-07-26 14:05:08 +0200

    Implemented the RandomChoiceStrategy.kt

--- src/main/kotlin/mg/rockpaperscissors/domain/strategy/RandomChoiceStrategy.kt
+++ src/main/kotlin/mg/rockpaperscissors/domain/strategy/RandomChoiceStrategy.kt
@@ -1,9 +1,15 @@
 package mg.rockpaperscissors.domain.strategy
 
 import mg.rockpaperscissors.domain.Action
+import kotlin.random.Random
 
-class RandomChoiceStrategy: Strategy {
-    override fun nextAction(): Action {
-        TODO("Not yet implemented")
-    }
+/**
+ * Implementing the Strategy Interface to determine a next Action according to
+ * the "Random Choice Strategy".
+ * The Random Choice strategy randomly returns Actions
+ */
+class RandomChoiceStrategy : Strategy {
+    private val moves = Action.values()
+
+    override fun nextAction(): Action = moves[Random.nextInt(moves.size)]
 }
