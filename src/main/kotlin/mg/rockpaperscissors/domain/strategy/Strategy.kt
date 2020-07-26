d52eecdae61348a9db040ef2c978f12137467b77
marcogramm <mail.mgramm@gmail.com>
2020-07-26 13:40:59 +0200

    Added Documentation for the Strategy Interface

--- src/main/kotlin/mg/rockpaperscissors/domain/strategy/Strategy.kt
+++ src/main/kotlin/mg/rockpaperscissors/domain/strategy/Strategy.kt
@@ -2,6 +2,14 @@ package mg.rockpaperscissors.domain.strategy
 
 import mg.rockpaperscissors.domain.Action
 
+/**
+ * Interface for choosing a next Action according to a given Stategy
+ * Implementing classes are free to decide which Action to return next.
+ */
 interface Strategy {
+    /**
+     * returns the next Action according to this Strategy
+     * @return The next Action
+     */
     fun nextAction(): Action
 }
