a7633288b97b8008f3dd07c26bb7707a273ab4c1
marcogramm <mail.mgramm@gmail.com>
2020-07-26 12:51:20 +0200

    implemented the beats method

--- src/main/kotlin/mg/rockpaperscissors/domain/Action.kt
+++ src/main/kotlin/mg/rockpaperscissors/domain/Action.kt
@@ -2,20 +2,23 @@ package mg.rockpaperscissors.domain
 
 enum class Action {
     ROCK {
-        override fun beats(action: Action): Boolean {
-            TODO("Not yet implemented")
+        override fun beats(action: Action) = when (action) {
+            SCISSORS -> true
+            else -> false
         }
     },
     SCISSORS {
-        override fun beats(action: Action): Boolean {
-            TODO("Not yet implemented")
+        override fun beats(action: Action) = when (action) {
+            PAPER -> true
+            else -> false
         }
     },
     PAPER {
-        override fun beats(action: Action): Boolean {
-            TODO("Not yet implemented")
+        override fun beats(action: Action) = when (action) {
+            ROCK -> true
+            else -> false
         }
     };
 
-    abstract fun beats(action: Action) : Boolean
+    abstract fun beats(action: Action): Boolean
 }
